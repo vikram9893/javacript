26dd6347ea1a03cdec178909363230d76b24d727
vikram9893 <124583140+vikram9893@users.noreply.github.com>
2024-01-16 05:06:45 +0000

    datatype

---
+++ 01_Basics/01_datatype.js
@@ -0,0 +1,26 @@
+"use strict" ;//treat all js code as newer version
+
+// alert(3+3) //we are using nodejs not browser
+
+console.log(3
+    +3)         //code readability should be high
+
+console.log("vikram")         
+
+
+let name="vikram"
+let age=18
+let isLoggedIn=false
+let state;
+
+//number=>2 to power 53
+//bigInt
+//string=""
+//Boolean=>true/false
+//null=>standalone value
+//undefined
+//symbol
+//object
+console.log(typeof null)
+
+
