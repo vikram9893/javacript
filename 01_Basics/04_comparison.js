21913f73266b7a1fbdba5f8a54f2e809396f7dfe
vikram9893 <124583140+vikram9893@users.noreply.github.com>
2024-01-18 07:12:56 +0000

    stack and heap memory

--- 01_Basics/04_comparison.js
+++ 01_Basics/04_comparison.js
@@ -1,3 +1,27 @@
-console.log(null>0);
-console.log(null==0);
-console.log(null>=0);
+
+
+
+
+
+
+//stack(primitive) and heap memory(non primitive)
+
+
+let myYoutubeChannel="hiteshchoudharydotcom"
+let anotherName= myYoutubeChannel
+anotherName="chaiaurcode"
+
+console.log(anotherName);
+console.log(myYoutubeChannel);
+
+let userOne={
+    email:"use@gmail.com",
+    upi:"user@ybl"
+   
+}
+let userTwo=userOne
+
+userTwo.email="hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
